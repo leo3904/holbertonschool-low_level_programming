e4c67d64f25af9ea43d5a9480293f8be3d008416
leo3904 <leonardoruiz1988@gmail.com>
2020-06-10 21:53:58 +0000

    Another commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,13 @@
 
 /**
  * main - Start point
- * printf - Print the string
- * return: 0
+ * printf: Print the string
+ *
+ * Return: Always (0)
  */
 
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
