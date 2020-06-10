c5689704189e56ad2accadfe7ab86dac36ebf584
leo3904 <leonardoruiz1988@gmail.com>
2020-06-10 20:17:18 +0000

    One more time

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+A#include <stdio.h>
 
 /**
  * main - Entry point
- *
+ * puts - Prints on the screen
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
