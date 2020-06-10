f960a8f5b6c73d7920f0ee38892ac2d4fa54b932
leo3904 <leonardoruiz1988@gmail.com>
2020-06-10 21:03:33 +0000

    Added printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - Start point
+ * printf - Print the string
+ * return: 0
+ */
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
