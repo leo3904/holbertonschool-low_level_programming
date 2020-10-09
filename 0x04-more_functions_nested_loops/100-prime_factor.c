d0782b1a0841fa0623599e4587bb9136cd7979f9
Leonardo <leonardoruiz1988@gmail.com>
2020-10-08 19:22:49 -0500

    Correction in number 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,53 +1,23 @@
 #include "holberton.h"
-#include <stdio.h>
-#include <math.h>
-
-#define NUM 612852475143
 
 /**
- * main - find the largest prime factor of 612852475143
+ * main - Return the largest prime factor for 612852475143 .
  *
- * Return: 0 success
+ * Return: Always 0.
  */
 int main(void)
 {
-	long i = 2, p = 0, n = NUM;
+	long int i;
+	long int n = 612852475143;
 
 	for (i = 2; i <= n; i++)
 	{
-		if (!(n % i) && is_prime(i))
+		if (n % i == 0)
 		{
 			n /= i;
-			if (i > p)
-			{
-				p = i;
-			}
-			if (is_prime(n))
-			{
-				p = n;
-				break;
-			}
-			i = 2;
+			i--;
 		}
 	}
-	printf("%ld\n", p);
+	printf("%ld\n", i);
 	return (0);
-}
-
-/**
- * is_prime - checks if an integer is prime
- * @n: the integer to check
- *
- * Return: 1 if prime, 0 otherwise
- */
-int is_prime(long n)
-{
-	int i;
-
-	if (n < 2)
-		return (0);
-	for (i = 2; i <= sqrt(n); i++)
-		if (!(n % i))
-			return (0);
-	return (1);
 }
