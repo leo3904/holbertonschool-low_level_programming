5c562e89dda835b2ad137f0ad3182b2df1978b78
Leonardo <leonardoruiz1988@gmail.com>
2021-03-23 17:45:30 -0500

    Corrected 100-main_opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,10 +1,40 @@
-#include "holberton.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
-  * main - define function
-  * @void - describe argument
-  * Return: what does it return?
-  */
-int main(void)
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array containing all arguments
+ *
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[])
 {
+	int bytes, index;
+	char *op_codes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	op_codes = (char *)main;
+
+	for (index = 0; index < bytes - 1; index++)
+		printf("%02hhx ", op_codes[index]);
+
+	printf("%02hhx\n", op_codes[index]);
+
 	return (0);
 }
