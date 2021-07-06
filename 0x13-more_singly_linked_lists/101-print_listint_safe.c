2041e20b9bbc2bb5d946717f3cc62e69f617fe92
Leonardo <leonardoruiz1988@gmail.com>
2021-07-06 16:08:51 -0500

    Single ll

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,26 +1,41 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_listint_safe - prints a list
- * @head: address of pointer to first node
- *
- * Return: address of head
+ * print_listint_safe - print a linked list only one time
+ * @head: head of LL
+ * Return: counter of nodes & prints an error if the linked list is a circle
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	if (head)
+	const listint_t *slow, *fast, *marker;
+	unsigned int counter = 0, flag = 0;
+
+	if (head == NULL)
+		return (0);
+	marker = slow = head;
+	fast = head->next;
+	while (head != NULL)
 	{
 		printf("[%p] %d\n", (void *)head, head->n);
-		if (head->next < head)
-		{
-			return (1 + print_listint_safe(head->next));
-		}
-		else
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			return (1);
-		}
+		head = head->next;
+		counter++;
 
+		if (flag == 0 && fast != NULL && fast->next != NULL && slow != NULL)
+		{
+			if (fast == slow)
+			{
+				flag = 1;
+				slow = marker;
+			}
+			fast = fast->next->next;
+		}
+		if (flag == 1 && slow == head)
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
+		}
+		slow = slow->next;
 	}
-	return (0);
+	return (counter);
 }
